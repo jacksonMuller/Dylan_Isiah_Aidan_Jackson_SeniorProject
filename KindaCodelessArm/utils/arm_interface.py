95eea2c0abf2463f6ee1814cb060a32f27ec9ac9
dylansuzuki <nalydikuzus@gmail.com>
2026-03-23 16:53:02 -0700

    Adapted simulator utils for use with hardware (untested)

---
+++ KindaCodelessArm/utils/arm_interface.py
@@ -0,0 +1,185 @@
+import argparse
+import sys
+import time
+import threading
+import math
+
+# keyboard input helpers (unix)
+import tty
+import termios
+from contextlib import contextmanager
+from lerobot.motors import Motor, MotorNormMode
+
+# Try to import lerobot motor bus classes (multiple possible import paths).
+MotorsBus = None
+FeetechMotorsBus = None
+_has_lerobot = False
+
+try:
+    # primary guess: recent lerobot layout
+    from lerobot.common.motors.motors_bus import MotorsBus
+    _has_lerobot = True
+except Exception:
+    try:
+        from lerobot.motors.feetech import FeetechMotorsBus
+        MotorsBus = FeetechMotorsBus
+        _has_lerobot = True
+    except Exception:
+        _has_lerobot = False
+
+if not _has_lerobot:
+    print("Warning: could not import lerobot MotorsBus classes. "
+          "The script will still run in 'dry' mode, but cannot send commands to the arm.\n"
+          "Install Lerobot (and the feetech extras) with:\n"
+          "  pip install -e '.[feetech]'\n"
+          "or follow the repo instructions: https://github.com/huggingface/lerobot\n")
+    MotorsBus = None  # keep name defined
+    
+DEFAULT_MOTORS={
+                "joint_1": Motor(1, "sts3215", MotorNormMode.RANGE_M100_100),
+                "joint_2": Motor(2, "sts3215", MotorNormMode.RANGE_M100_100),
+                "joint_3": Motor(3, "sts3215", MotorNormMode.RANGE_M100_100),
+                "joint_4": Motor(4, "sts3215", MotorNormMode.RANGE_M100_100),
+                "joint_5": Motor(5, "sts3215", MotorNormMode.RANGE_M100_100),
+                "joint_6": Motor(6, "sts3215", MotorNormMode.RANGE_M100_100),
+            }
+
+
+class RobotMotorInterface:
+    """
+    RobotMotorInterface: Main interface helper class that contains easy abstractions
+                         for connecting to and controlling the arm.
+    """
+    def __init__(self, port="/dev/ttyACM0", motors=DEFAULT_MOTORS):
+        """
+        port: port of motor control board.  Use lerobot calibration script to find correct port.  For more instructions see the huggingface website.
+        motors: mapping of motor names to motor objects
+        """
+        self.port = port
+        self.motors = motors
+        self.connected = False
+        self.bus = None
+        
+        self.joint_names = {
+            1: "joint_1",
+            2: "joint_2",
+            3: "joint_3",
+            4: "joint_4",
+            5: "joint_5",
+            6: "joint_6",
+        }
+        self.wave_starting_position = {
+            self.joint_names[1]: 2175,
+            self.joint_names[2]: 1354,
+            self.joint_names[3]: 2014,
+            self.joint_names[4]: 1545,
+            self.joint_names[5]: 3786,
+            self.joint_names[6]: 2395,
+        }
+        self.rest_position = {
+            self.joint_names[1]: 2009,
+            self.joint_names[2]: 888,
+            self.joint_names[3]: 2990,
+            self.joint_names[4]: 2792,
+            self.joint_names[5]: 2984,
+            self.joint_names[6]: 2045,
+        }
+
+        # Attempt to instantiate a motors bus in a few ways:
+        try:
+            # common constructor pattern: MotorsBus(port=..., brand="feetech", model="sts3215", baudrate=...)
+            # This may differ by lerobot version. Adjust if your installation uses another signature.
+            try:
+                self.bus = MotorsBus(port=self.port, motors=self.motors, brand="feetech", model="sts3215")
+            except TypeError:
+                # alternative: simpler signature
+                print("Trying simpler signature")
+                self.bus = FeetechMotorsBus(self.port, motors=self.motors)
+                self.bus.connect()
+            self.connected = True
+            print(f"Connected to Motors bus on {self.port}.")
+        except Exception as e:
+            print("ERROR connecting to MotorsBus:", e)
+
+    def cleanup(self):
+        print("\r\n====================\r\nDisconnecting from arm\r\n====================")
+        self.rest_arm()
+        self.bus.disconnect()
+
+    def motor_control(self, motor, offset):
+        """
+            Ultra-simple generic motor control function with no smoothing.
+            Controls one motor at a time and moves it by a given offset
+
+            Arguments:
+                - motor: number corresponding to a motor in joint_names
+                - offset: change in position you want to give the motor
+        """
+        motor_pos = self.bus.read("Present_Position", self.joint_names[motor], normalize=False)
+        print("\r\nCurrent position of", self.joint_names[motor], motor_pos)
+        self.bus.write("Goal_Position", self.joint_names[motor], motor_pos+offset, normalize=False)
+
+    def wave_hand(self):
+        """
+            Goes to hardcoded wave position and then moves wrist back and forth to "wave"
+            Positions for raised hand:
+                Joint_1: 2175
+                Joint_2: 1354
+                Joint_3: 2014
+                Joint_4: Moving joint
+                Joint_5: 3786
+                Joint_6: 2395
+        """
+        print("\r\nStarting Wave")
+        self.bus.sync_write("Goal_Position", self.wave_starting_position, normalize=False)
+        time.sleep(1)
+        j4_position = self.bus.read("Present_Position", "joint_4", normalize=False)
+        for _ in range(3):
+            print("\r\nWaving Hand")
+            self.bus.write("Goal_Position", "joint_4", j4_position+200, normalize=False)
+            time.sleep(1)
+            self.bus.write("Goal_Position", "joint_4", j4_position-200, normalize=False)
+            time.sleep(1)
+        self.bus.write("Goal_Position", "joint_4", j4_position, normalize=False)
+        
+    def rest_arm(self):
+        """
+        Goes to hardcoded resting position
+        """
+        print("\r\nGoing to rest")
+        self.bus.sync_write("Goal_Position", self.rest_position, normalize=False)
+
+    def move_to_pose(self, desired_position, duration=0):
+        """
+        Generic position input that should smooth the transition based on duration argument
+        Arguments:
+            - desired_position: dictionary with the target position, 
+              see self.starting_position or self.rest_position for examples
+            - duration: duration of movement, leave at 0 to just go immediately
+        """
+
+        if duration == 0:
+            # Just go as fast as possible
+            self.bus.sync_write("Goal_Position", desired_position, normalize=False)
+        else:
+            # Interpolate positions based on given duration
+            start_time = time.time()
+            starting_pose = self.bus.read("Present_Position", "joint_4", normalize=False)
+            
+            while True:
+                t = time.time() - start_time
+                if t > duration:
+                    break
+
+                # Interpolation factor [0,1] (make sure it doesn't exceed 1)
+                alpha = min(t / duration, 1)
+
+                # Interpolate each joint
+                position_dict = {}
+                for joint in desired_position:
+                    p0 = starting_pose[joint]
+                    pf = desired_position[joint]
+                    position_dict[joint] = (1 - alpha) * p0 + alpha * pf
+
+                # Send command
+                self.bus.sync_write("Goal_Position", position_dict, normalize=False)
---
+++ KindaCodelessArm/utils/so101_forward_kinematics.py
@@ -0,0 +1,80 @@
+import time
+import numpy as np
+
+def Rx(thetadeg):
+    thetarad = np.deg2rad(thetadeg)
+    c = np.cos(thetarad)
+    s = np.sin(thetarad)
+    return np.array([[1, 0, 0],
+                     [0, c, -s],
+                     [0, s, c]])
+
+def Ry(thetadeg):
+    thetarad = np.deg2rad(thetadeg)
+    c = np.cos(thetarad)
+    s = np.sin(thetarad)
+    return np.array([[c, 0, s],
+                     [0, 1, 0],
+                     [-s, 0, c]])
+    
+def Rz(thetadeg):
+    thetarad = np.deg2rad(thetadeg)
+    c = np.cos(thetarad)
+    s = np.sin(thetarad)
+    return np.array([[c, -s, 0],
+                     [s, c, 0],
+                     [0, 0, 1]])
+
+# Displacement and rotation based on diagram at https://maegantucker.com/ECE4560/assets/assignment7/new-config.png
+# Some of these values will need to be inverted based on testing
+# Based on the assumption that x = red, y = green, z = blue
+
+
+# NOTE: This has only been tested on the simulator, not with the physical arm.
+def get_gw1(theta1_deg):
+    displacement = (0.0388353, 0.0, 0.0624) # translation from world frame to shoulder_pan motor
+    rotation = Rx(180) @ Rz(180) @ Rz(theta1_deg) #Rotate 180 degrees in the z and x axes to account for the orientation of the motor in relation to the world frame, then rotate by theta1 around the z axis
+    pose = np.block([[rotation, np.array(displacement).reshape(3,1)], [0, 0, 0, 1]])
+    return pose
+
+def get_g12(theta2_deg):
+    displacement = (-0.0303992, 0.0,  -0.0542) # translation from shoulder_pan motor to shoulder_lift motor
+    rotation = Rz(-90) @ Ry(270) @ Rz(theta2_deg) # rotate from shoulder_pan frame to shoulder_lift frame, then rotate by theta2 around the z axis
+    pose = np.block([[rotation, np.array(displacement).reshape(3,1)], [0, 0, 0, 1]])
+    return pose
+
+def get_g23(theta3_deg):
+    displacement = (-0.11257, -0.028, 0.0) # translation from shoulder_lift motor to elbow_flex motor
+    rotation = Rz(90) @ Rz(theta3_deg) # rotate from shoulder_lift frame to elbow_flex frame, then rotate by theta3 around the y axis
+    pose = np.block([[rotation, np.array(displacement).reshape(3,1)], [0, 0, 0, 1]])
+    return pose
+
+def get_g34(theta4_deg):
+    displacement = (-0.14, 0.0, 0.0) # translation from elbow_flex motor to wrist_flex motor - 2/24/26 changed from 0.1349 to .14 and the simulation looks more accurate
+    rotation = Rz(-90) @ Rz(theta4_deg) # rotate from elbow_flex frame to wrist_flex frame, then rotate by theta4 around the y axis
+    pose = np.block([[rotation, np.array(displacement).reshape(3,1)], [0, 0, 0, 1]])
+    return pose
+
+def get_g45(theta5_deg):
+    displacement = (0.0, -0.0611, 0.0) # translation from wrist_flex motor to wrist_roll motor (assumed to be zero since they are co-located)
+    rotation = Rz(180) @ Rx(90) @ Rz(theta5_deg) # rotate from wrist_flex frame to wrist_roll frame, then rotate by theta5 around the z axis
+    pose = np.block([[rotation, np.array(displacement).reshape(3,1)], [0, 0, 0, 1]])
+    return pose
+
+def get_g5t():
+    displacement = (0.0, 0.0, -0.1034) # translation from wrist_roll motor to object frame
+    rotation = Rx(90) # No rotation from wrist roll frame to object frame in new config
+    pose = np.block([[rotation, np.array(displacement).reshape(3,1)], [0, 0, 0, 1]])
+    return pose
+
+def get_forward_kinematics(position_dict):
+    gw1 = get_gw1(position_dict['shoulder_pan'])
+    g12 = get_g12(position_dict['shoulder_lift'])
+    g23 = get_g23(position_dict['elbow_flex'])
+    g34 = get_g34(position_dict['wrist_flex'])
+    g45 = get_g45(position_dict['wrist_roll'])
+    g5t = get_g5t()
+    gwt = gw1 @ g12 @ g23 @ g34 @ g45 @ g5t
+    position = gwt[0:3, 3]
+    rotation = gwt[0:3, 0:3]
+    return position, rotation
